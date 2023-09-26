87fd84b5d947d15b7e91b61a10ebac2b559565c2
Fidelis <yonlifidelis2@gmail.com>
2023-09-26 19:07:51 +0200

    Batch Processing

--- FonctionProcessFile.py
+++ FonctionProcessFile.py
@@ -2,18 +2,6 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import year, month, dayofmonth, when
 from pyspark.sql import functions as F
 
-# Créez la session Spark
-spark = SparkSession.builder \
-    .appName("PublicTransport") \
-    .getOrCreate()
-
-# Spécifiez le chemin du répertoire raw dans Azure Data Lake Storage Gen2
-raw_data_path = "abfss://datalake@fidelistockage.dfs.core.windows.net/raw/"
-
-# Utilisez dbutils.fs.ls pour lister les fichiers CSV bruts
-raw_files = dbutils.fs.ls(raw_data_path)
-raw_csv_files = [f.path for f in raw_files if f.name.endswith(".csv")]
-
 # Fonction pour effectuer les transformations sur un fichier
 def process_file(csv_file):
     # Chargez le fichier CSV brut dans un DataFrame
@@ -46,11 +34,3 @@ def process_file(csv_file):
 
     return transformed_data
 
-# # Boucle sur les fichiers pour effectuer les transformations sur chaque fichier
-# for csv_file in raw_csv_files:
-#     transformed_data = process_file(csv_file)
-#     # Enregistrez le DataFrame transformé dans un répertoire de données traitées
-#     transformed_data.write.mode("overwrite").csv("abfss://datalake@fidelistockage.dfs.core.windows.net/processed/transformed_data_" + csv_file)
-
-# # Fermez la session Spark
-# spark.stop()
