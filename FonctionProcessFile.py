390fb38d73a277dd0b672e8b69bee7ea918d51ef
Fidelis <yonlifidelis2@gmail.com>
2023-09-27 17:26:52 +0200

    Politiques de Conservation

--- FonctionProcessFile.py
+++ FonctionProcessFile.py
@@ -1,36 +1,37 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import year, month, dayofmonth, when
-from pyspark.sql import functions as F
+import os
+import time
 
-# Fonction pour effectuer les transformations sur un fichier
-def process_file(csv_file):
-    # Chargez le fichier CSV brut dans un DataFrame
-    raw_data = spark.read.format('csv').option('header', True).option('delimiter', ',').load(csv_file)
+raw_csv_files = [f.path for f in raw_files if f.name.endswith(".csv")]
 
-    # Transformations de Date: Extraire l'année, le mois, le jour et le jour de la semaine de la date
-    transformed_data = raw_data.withColumn("Annee", year("Date")) \
-                               .withColumn("Mois", month("Date")) \
-                               .withColumn("Jour", dayofmonth("Date"))
-
-    # Catégorisation des retards
-    transformed_data = transformed_data.withColumn("CatégorieRetard",
-        when(transformed_data["Delay"] <= 0, "Pas de Retard")
-        .when((transformed_data["Delay"] >= 1) & (transformed_data["Delay"] <= 10), "Retard Court")
-        .when((transformed_data["Delay"] >= 11) & (transformed_data["Delay"] <= 20), "Retard Moyen")
-        .when(transformed_data["Delay"] > 20, "Long Retard")
-        .otherwise("Autre"))
-
-    # Détermination des heures de pointe
-    seuil_passagers_pointe = 75
-    transformed_data = transformed_data.withColumn("HeureDePointe",
-        when(transformed_data["Passengers"] > seuil_passagers_pointe, "Oui")
-        .otherwise("Non"))
+# Définissez le nombre de fichiers à traiter à chaque itération
+batch_size = 2
+count = 1
+while raw_csv_files:
+    # Prenez les prochains fichiers à traiter dans la liste
+    batch_files = raw_csv_files[:batch_size]
     
-    # Analyse des itinéraires
-    transformed_data = transformed_data.groupBy("Route").agg(
-        F.mean("Delay").alias("RetardMoyen"),
-        F.mean("Passengers").alias("NombreMoyenPassagers"),
-        F.count("*").alias("NombreTotalVoyages"))
+    # Mettez à jour la liste des fichiers restants
+    raw_csv_files = raw_csv_files[batch_size:]
+    
+    # Boucle sur les fichiers pour effectuer les transformations sur chaque fichier
+    for csv_file in batch_files:
+        
+        # Chargez le fichier CSV brut dans un DataFrame
+        raw_data = spark.read.format('csv').option('header', True).option('delimiter', ',').load(csv_file)
+        
+        analytical_data = process_file(csv_file,count)
+        count = count + 1
+        print(analytical_data)
+        # Extrayez le nom du fichier (data2.csv) à partir du chemin complet
+        filename = os.path.basename(csv_file)
+        # print(filename)
+        # Enregistrez le DataFrame transformé dans un répertoire de données traitées avec le nom de fichier
+        analytical_data.write.mode("overwrite").option("header", "true").csv(processed_path + "analyse/" + filename)
 
-    return transformed_data
 
+    # Attendez 15 secondes avant de traiter le prochain lot de fichiers
+    print("Attendez 15 secondes avant de traiter le prochain lot de fichiers !")
+    time.sleep(15)
+
+# Fermez la session Spark
+# spark.stop()
